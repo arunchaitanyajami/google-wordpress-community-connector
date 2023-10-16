2e51afcc0c3dec236a96604d970c0edfebb98b07
Arun Chaitanya Jami <arun.chaitanyajami@news.co.uk>
2023-10-16 21:34:06 +0530

    Data referesh

--- src/main.js
+++ src/main.js
@@ -88,7 +88,7 @@ function createField(fields, types, key, value) {
 
     field.setType(semanticType);
     field.setId(key.replace(/\s/g, '_').toLowerCase());
-    field.setDescription( isNumeric(value) + " >> " + typeof value + " >> " + Object.values( value )[0] );
+    field.setDescription( isNumeric(value) + " >> " + typeof value + " >> " + Object.keys( value )[0] );
     field.setName(key);
 }
 
