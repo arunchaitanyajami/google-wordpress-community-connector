06602bf26bd273deb75cf3d804f271cd0e37de9c
Arun Chaitanya Jami <arun.chaitanyajami@news.co.uk>
2023-10-16 23:23:06 +0530

    Fix object issue

--- src/main.js
+++ src/main.js
@@ -86,8 +86,6 @@ function createField(fields, types, key, value) {
     var semanticType = getSemanticType(value, types);
     var field = semanticType === types.NUMBER ? fields.newMetric() : fields.newDimension();
 
-    sendUserError( key + " ==== " +JSON.stringify(value));
-
     field.setType(semanticType);
     field.setId(key.replace(/\s/g, '_').toLowerCase());
     field.setDescription(isNumeric(value) + " >> " + typeof value + " >> " + key);
