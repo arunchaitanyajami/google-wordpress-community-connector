412fd046399d15746b5ceced5bc52b287fed3969
Arun Chaitanya Jami <arun.chaitanyajami@news.co.uk>
2023-10-17 06:15:44 +0530

    URL TYPE CHECK

--- src/main.js
+++ src/main.js
@@ -113,7 +113,7 @@ function createField(fields, types, key, value) {
 
     field.setType(semanticType);
     field.setId(key.replace(/\s/g, '_').toLowerCase());
-    field.setDescription( key );
+    field.setDescription(key + ' >> ' + validURL(value));
     field.setName(key);
 }
 
